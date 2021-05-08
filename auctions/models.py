13f91b4d0cb9deea5c39471ff11b7936ea88e5e9
GretaRob <greta.robichaud@gmail.com>
2021-05-08 22:20:40 +0700

    added Listings model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    image = models.ImageField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return str(self.name)
